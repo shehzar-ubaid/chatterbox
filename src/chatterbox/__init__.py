c861bff009103f22cfbc5b49b9e2b06bab310d2b
shehzar-ubaid <faisalubaid22@gmail.com>
2026-03-12 00:47:47 +0500

    Fixed init import names and version

--- src/chatterbox/__init__.py
+++ src/chatterbox/__init__.py
@@ -1,11 +1,9 @@
-try:
-    from importlib.metadata import version
-except ImportError:
-    from importlib_metadata import version  # For Python <3.8
-
-#__version__ = "1.0.0"
-
+# Version check bypass kar diya taake metadata error na aye
+__version__ = "1.0.0"
 
 from .tts import ChatterboxTTS
 from .vc import ChatterboxVC
 from .mtl_tts import ChatterboxMultilingualTTS, SUPPORTED_LANGUAGES
+
+# FIX: handler.py ko crash se bachanay kay liye alias bana diya
+Chatterbox = ChatterboxTTS
